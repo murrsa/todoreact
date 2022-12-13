55e67a961d60dec38a519fc78764d1df0818f10d
Samantha <113162205+murrsa@users.noreply.github.com>
2022-12-12 18:54:26 -0800

    Add files via upload

---
+++ src/Task.jsx
@@ -0,0 +1,15 @@
+import './App'
+
+export const Task = (props) => {
+    return(
+        <div 
+        className="task" 
+        style={{backgroundColor: props.completed ? "green" : "white" }}
+        >
+        <h1>{props.taskName}</h1>
+        <button onClick={() => props.completeTask(props.id)}> taskName </button>
+        <button onClick={() => props.deleteTask(props.id)}> X </button>
+
+        </div>
+    );
+};
